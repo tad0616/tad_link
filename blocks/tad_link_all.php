9039bcbbf4b293021a14f30bcb9b3d92e77d4a79
Tad <tad0616@gmail.com>
2013-10-08 08:32:47 +0800

    以#取代沒有值的連結網址

--- blocks/tad_link_all.php
+++ blocks/tad_link_all.php
@@ -1,20 +1,15 @@
 <?php
-//  ------------------------------------------------------------------------ //
-// 本模組由 tad 製作
-// 製作日期：2011-11-14
-// $Id:$
-// ------------------------------------------------------------------------- //
 
 //區塊主函式 (好站推薦快速連結(tad_link_all))
 function tad_link_all($options){
   global $xoopsDB;
 
-	$block="";
-	$and_cate=empty($options[1])?"":"where cate_sn in({$options[1]})";
-	$sql = "select * from ".$xoopsDB->prefix("tad_link_cate")." $and_cate order by of_cate_sn,cate_sort";
-	$result = $xoopsDB->query($sql) or redirect_header($_SERVER['PHP_SELF'],3, mysql_error());
-	while($all=$xoopsDB->fetchArray($result)){
-	  //以下會產生這些變數： $cate_sn , $of_cate_sn , $cate_title , $cate_sort
+  $block="";
+  $and_cate=empty($options[1])?"":"where cate_sn in({$options[1]})";
+  $sql = "select * from ".$xoopsDB->prefix("tad_link_cate")." $and_cate order by of_cate_sn,cate_sort";
+  $result = $xoopsDB->query($sql) or redirect_header($_SERVER['PHP_SELF'],3, mysql_error());
+  while($all=$xoopsDB->fetchArray($result)){
+    //以下會產生這些變數： $cate_sn , $of_cate_sn , $cate_title , $cate_sort
     foreach($all as $k=>$v){
       $$k=$v;
     }
@@ -23,8 +18,8 @@ function tad_link_all($options){
     $link_js=($options[0]==1)?"window.open(this.value,'_blank');":"location.href='".XOOPS_URL."/modules/tad_link/index.php?link_sn='+this.value";
 
 
-  	$sql2 = "select * from ".$xoopsDB->prefix("tad_link")." where `cate_sn` = '{$cate_sn}' and enable='1' order by link_sort";
-  	$result2 = $xoopsDB->query($sql2) or redirect_header($_SERVER['PHP_SELF'],3, mysql_error());
+    $sql2 = "select * from ".$xoopsDB->prefix("tad_link")." where `cate_sn` = '{$cate_sn}' and enable='1' order by link_sort";
+    $result2 = $xoopsDB->query($sql2) or redirect_header($_SERVER['PHP_SELF'],3, mysql_error());
     $total=$xoopsDB->getRowsNum($result2);
     if(empty($total))continue;
 
@@ -32,12 +27,13 @@ function tad_link_all($options){
     $block['link_js']=$link_js;
     $block['color']=$color;
     $i=0;
-  	while($all2=$xoopsDB->fetchArray($result2)){
-  	  //以下會產生這些變數： $link_sn , $cate_sn , $link_title , $link_url , $link_desc , $link_sort , $link_counter , $unable_date , $uid , $post_date , $enable
+    while($all2=$xoopsDB->fetchArray($result2)){
+      //以下會產生這些變數： $link_sn , $cate_sn , $link_title , $link_url , $link_desc , $link_sort , $link_counter , $unable_date , $uid , $post_date , $enable
       foreach($all2 as $k=>$v){
         $$k=$v;
       }
       $val=($options[0]==1)?$link_url:$link_sn;
+      if(empty($val))$val="#";
       $block['cate_title'][$i]['link_title']=$link_title;
       $block['cate_title'][$i]['val']=$val;
       $i++;
@@ -45,28 +41,28 @@ function tad_link_all($options){
 
 
   }
-	return $block;
+  return $block;
 }
 
 //區塊編輯函式
 function tad_link_all_edit($options){
 
   include_once XOOPS_ROOT_PATH."/modules/tad_link/function_block.php";
-	$chked0_0=($options[0]=="1")?"checked":"";
-	$chked0_1=($options[0]=="0")?"checked":"";
+  $chked0_0=($options[0]=="1")?"checked":"";
+  $chked0_1=($options[0]=="0")?"checked":"";
 
-	$menu=block_link_cate($options[1]);
+  $menu=block_link_cate($options[1]);
 
-	$form="{$menu['js']}
-	"._MB_TADLINK_TAD_LINK_ALL_EDIT_BITEM0."
-	<INPUT type='radio' $chked0_0 name='options[0]' value='1'>"._YES."
-	<INPUT type='radio' $chked0_1 name='options[0]' value='0'>"._NO."
+  $form="{$menu['js']}
+  "._MB_TADLINK_TAD_LINK_ALL_EDIT_BITEM0."
+  <INPUT type='radio' $chked0_0 name='options[0]' value='1'>"._YES."
+  <INPUT type='radio' $chked0_1 name='options[0]' value='0'>"._NO."
 
-	"._MB_TADLINK_TAD_CATE_MENU."
-	{$menu['form']}
-	<INPUT type='hidden' name='options[1]' id='bb' value='{$options[1]}'>
-	";
-	return $form;
+  "._MB_TADLINK_TAD_CATE_MENU."
+  {$menu['form']}
+  <INPUT type='hidden' name='options[1]' id='bb' value='{$options[1]}'>
+  ";
+  return $form;
 }
 
 if(!function_exists('cate_sn2color')){
