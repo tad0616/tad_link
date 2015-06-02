dcbad52aa4b703be9d38085e1ac68a2febe03462
mambax7 <mambax7@gmail.com>
2015-06-02 04:02:30 -0400

    Adding English

---
+++ language/english/blocks.php
@@ -0,0 +1,28 @@
+<?php
+//define('_MB_TADLINK_SHOW_EDIT_BITEM0',"顯示數量");
+//define('_MB_TADLINK_SHOW_EDIT_BITEM1',"顯示縮圖？");
+//define('_MB_TADLINK_SHOW_EDIT_BITEM2',"顯示站名？");
+//define('_MB_TADLINK_SHOW_EDIT_BITEM3',"顯示網址？");
+//define('_MB_TADLINK_SHOW_EDIT_BITEM4',"顯示最新");
+//define('_MB_TADLINK_SHOW_EDIT_BITEM5',"隨機顯示");
+//define('_MB_TADLINK_SHOW_EDIT_BITEM7',"按照排序");
+//define('_MB_TADLINK_SHOW_EDIT_BITEM6',"直接外連？");
+//define('_MB_TADLINK_SHOW_SORT',"排序方式？");
+//define('_MB_TADLINK_SHOW_HEIGHT',"每列高度：");
+//
+//define('_MB_TADLINK_TADLINK_ALL_EDIT_BITEM0',"直接外連？");
+//define('_MB_TADLINK_TAD_CATE_MENU',"選擇欲顯示之分類（可複選）");
+
+define('_MB_TADLINK_SHOW_EDIT_BITEM0', 'Display the Count number?');
+define('_MB_TADLINK_SHOW_EDIT_BITEM1', 'Display the thumbnail?');
+define('_MB_TADLINK_SHOW_EDIT_BITEM2', 'Display the Website name?');
+define('_MB_TADLINK_SHOW_EDIT_BITEM3', 'Display URL?');
+define('_MB_TADLINK_SHOW_EDIT_BITEM4', 'Show the latest');
+define('_MB_TADLINK_SHOW_EDIT_BITEM5', 'Shuffle');
+define('_MB_TADLINK_SHOW_EDIT_BITEM7', 'Sort');
+define('_MB_TADLINK_SHOW_EDIT_BITEM6', 'Directly outside even?');
+define('_MB_TADLINK_SHOW_SORT', 'Sort?');
+define('_MB_TADLINK_SHOW_HEIGHT', 'Height of each column:');
+
+define('_MB_TADLINK_TADLINK_ALL_EDIT_BITEM0', 'Directly outside even?');
+define('_MB_TADLINK_TAD_CATE_MENU', 'To select the display of classification (multiple choice)');
---
+++ language/english/main.php
@@ -0,0 +1,41 @@
+<?php
+//  ------------------------------------------------------------------------ //
+// 本模組由 tad 製作
+// 製作日期：2011-11-14
+// $Id:$
+// ------------------------------------------------------------------------- //
+
+define('_TAD_NEED_TADTOOLS', "This module needs TadTools module. You can download TadTools from <a href='http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50' target='_blank'>Tad's web</a>.");
+
+define('_MD_TADLINK_SMNAME1', 'Tad Links');
+
+//index.php
+define('_MD_TADLINK_LINK_SN', 'Link number');
+define('_MD_TADLINK_CATE', 'Categories');
+define('_MD_TADLINK_LINK_TITLE', 'Site Information');
+define('_MD_TADLINK_LINK_URL', 'Link');
+define('_MD_TADLINK_LINK_DESC', 'Site description');
+define('_MD_TADLINK_LINK_SORT', 'Links sort');
+define('_MD_TADLINK_LINK_COUNTER', 'Popular');
+define('_MD_TADLINK_UNABLE_DATE', 'Unable Day');
+define('_MD_TADLINK_UID', 'Author');
+define('_MD_TADLINK_POST_DATE', 'Post Date');
+define('_MD_TADLINK_ENABLE', 'Enabled');
+define('_MD_TADLINK_FORM', 'Link management');
+define('_MD_TADLINK_QUICK_ADD', 'Quick Add');
+define('_MD_TADLINK_GET_PIC', 'Re-capture');
+define('_MD_TADLINK_SHOW_CATE', 'Show Category (Sorting?)');
+define('_MD_TADLINK_GOTO_LINK', 'Connect to:');
+define('_MD_TADLINK_CATE_INFO', 'Detail');
+define('_MD_TADLINK_CATE_INFO2', 'List');
+define('_MD_TADLINK_AUTOGET', 'Auto-capture');
+define('_MD_TADLINK_SELECTED', 'Select');
+define('_MD_TADLINK_ADD_NEW_CATE', 'There is no category, please create a new category');
+define('_MD_TADLINK_NEW_CATE', 'Create a new classification category on the left');
+define('_MD_TADLINK_CATE_COUNT', 'There are %s link');
+
+define('_MD_TADLINK_BATCH', 'Batch Management');
+define('_MD_TADLINK_BATCH_DEL', 'Batch delete');
+define('_MD_TADLINK_BACK', 'Back');
+define('_MD_TADLINK_SELECT_ALL', 'Select');
+define('_MD_TADLINK_DRAG_SORT', 'Sort by Directly drag');
