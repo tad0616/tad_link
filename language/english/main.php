9fe826145bd8c1df46c35997d3372d7aba194797
Tad <tad0616@gmail.com>
2019-04-30 12:43:48 +0800

    加語系

--- language/english/main.php
+++ language/english/main.php
@@ -1,4 +1,5 @@
 <?php
+xoops_loadLanguage('main', 'tadtools');
 define('_TAD_NEED_TADTOOLS', 'This module needs TadTools module. You can download TadTools from <a href="http://campus-xoops.tn.edu.tw/modules/tad_modules/index.php?module_sn=1" target="_blank">XOOPS EasyGO</a>.');
 
 define('_MD_TADLINK_SMNAME1', 'Tad Links');
